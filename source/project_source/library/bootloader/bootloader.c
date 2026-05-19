a16dfe144870c070e9ea5f116d214b55b8f1f1a2
zhangsenjia <zhangsenjia@qimingxin.com>
2026-05-19 14:02:36 +0800

    1. 增加功能受限逻辑

--- source/project_source/library/bootloader/bootloader.c
+++ source/project_source/library/bootloader/bootloader.c
@@ -123,10 +123,10 @@ void boot_set_chip_capability(void)
     rom_hw_flash_read_security_mem(EN_FLASH_SEC_MEM1, 0, (uint8_t *)&g_chip_cap, sizeof(g_chip_cap));
     dump_u8buf("g_chip_cap", (uint8_t *)&g_chip_cap, sizeof(g_chip_cap));
 
-    g_chip_cap.tof_rang_limit_en = (g_chip_cap.tof_rang_limit_en & 0x1);
-    g_chip_cap.cad_limit_en = (g_chip_cap.cad_limit_en & 0x1);
-    g_chip_cap.buf_limit_en = (g_chip_cap.buf_limit_en & 0x1);
-    g_chip_cap.sfbw_limit_en = (g_chip_cap.sfbw_limit_en & 0x1);
+    g_chip_cap.tof_rang_limit_en = (g_chip_cap.tof_rang_limit_en & 0x1) ? 1 : 0;
+    g_chip_cap.cad_limit_en = (g_chip_cap.cad_limit_en & 0x1) ? 1 : 0;
+    g_chip_cap.buf_limit_en = (g_chip_cap.buf_limit_en & 0x1) ? 1 : 0;
+    g_chip_cap.sfbw_limit_en = (g_chip_cap.sfbw_limit_en & 0x1) ? 1 : 0;
     g_chip_cap.buf_limit_val = (g_chip_cap.buf_limit_val < 0x1) ? 0x1 : g_chip_cap.buf_limit_val;
     g_chip_cap.buf_limit_val = (g_chip_cap.buf_limit_val > 0x180) ? 0x180 : g_chip_cap.buf_limit_val;
     g_chip_cap.bw_high_limit_val = (g_chip_cap.bw_high_limit_val > 0x6) ? 0x0 : g_chip_cap.bw_high_limit_val;
@@ -139,9 +139,9 @@ void boot_set_chip_capability(void)
                     (g_chip_cap.buf_limit_en << 27) | (g_chip_cap.sfbw_limit_en << 26) |
                     (g_chip_cap.buf_limit_val << 16) | (g_chip_cap.bw_high_limit_val << 12) |
                     (g_chip_cap.bw_low_limit_val << 8) | (g_chip_cap.sf_high_limit_val << 4) |
-                    (g_chip_cap.sf_low_limit_val << 0));
+                    (g_chip_cap.sf_low_limit_val << 0)) & 0x3DFFFFFF;
 
-    write32(ADDR_SYS_CTRL_BASE + 0x5C, chip_cap_val);
+    write32(ADDR_SYS_CTRL_BASE + 0x60, chip_cap_val);
 }
 
 uint8_t rom_get_safe_level(void)
